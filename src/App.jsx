fd7560518055df715589acb07dfd575feda93c05
Hugo Gonçalves <hugomiguel2001.13@gmail.com>
2023-11-26 12:46:14 +0000

    Installed and configured Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <p>Yummy</p>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   );
 }
