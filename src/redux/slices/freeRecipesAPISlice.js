ac99c4dfb04291be6a7f51e4b0d32aa0edbef902
Hugo Gonçalves <hugomiguel_2001_@sapo.pt>
2023-11-28 19:37:36 +0000

    Refactored Recipes API Slice to extract repeated logic

--- src/redux/slices/freeRecipesAPISlice.js
+++ src/redux/slices/freeRecipesAPISlice.js
@@ -3,6 +3,22 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const appId = import.meta.env.VITE_EDAMAM_APP_ID;
 const apiKey = import.meta.env.VITE_EDAMAM_API_KEY;
 
+const transformRecipesResponse = (response) => {
+  const responseHitsArray = response.hits;
+
+  return responseHitsArray.map((hit) => {
+    const recipe = hit.recipe;
+    const recipeId = hit._links.self.href.split("/").pop().split("?")[0];
+
+    return {
+      id: recipeId,
+      title: recipe.label,
+      description: recipe.ingredientLines.slice(0, 2).join(", "),
+      image: recipe.image
+    };
+  });
+};
+
 export const freeRecipesAPISlice = createApi({
   reducerPath: "recipesAPISlice",
   baseQuery: fetchBaseQuery({
@@ -24,19 +40,7 @@ export const freeRecipesAPISlice = createApi({
         };
       },
       transformResponse: (response) => {
-        const responseHitsArray = response.hits;
-
-        return responseHitsArray.map((hit) => {
-          const recipe = hit.recipe;
-          const recipeId = hit._links.self.href.split("/").pop().split("?")[0];
-
-          return {
-            id: recipeId,
-            title: recipe.label,
-            description: recipe.ingredientLines.slice(0, 2).join(", "),
-            image: recipe.image
-          };
-        });
+        return transformRecipesResponse(response);
       },
     }),
     getRecipeCategories: builder.query({
@@ -68,6 +72,22 @@ export const freeRecipesAPISlice = createApi({
         });
       },
     }),
+    getRecipesByQuery: builder.query({
+      query: (query) => {
+        return {
+          url: "/api/recipes/v2",
+          params: {
+            type: "public",
+            app_id: appId,
+            app_key: apiKey,
+            q: query
+          }
+        };
+      },
+      transformResponse: (response) => {
+        return transformRecipesResponse(response);
+      }
+    })
   }),
 });
 
