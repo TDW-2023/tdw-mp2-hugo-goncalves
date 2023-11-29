a7ddee21947c3754764627438cfaa799a9ff2213
Hugo Gonçalves <hugomiguel_2001_@sapo.pt>
2023-11-29 16:19:25 +0000

    Created component to display Recipe's group of tags

--- src/components/Recipes/Tags/RecipeTag.test.jsx
+++ src/components/Recipes/Tags/RecipeTag.test.jsx
@@ -1,20 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import RecipeTag from "./RecipeTag.jsx";
 
-describe('RecipeTag component', () => {
-  it('renders the tag text correctly', () => {
+describe("RecipeTag component", () => {
+  it("renders the tag text correctly", () => {
     render(<RecipeTag tag="Vegan" />);
     const tagElement = screen.getByText(/Vegan/i);
     expect(tagElement).toBeInTheDocument();
   });
 
-  it('does not render when no tag is provided', () => {
+  it("does not render when no tag is provided", () => {
     render(<RecipeTag />);
     const tagElement = screen.queryByText(/Vegan/i);
     expect(tagElement).not.toBeInTheDocument();
   });
 
-  it('renders correctly with different tag text', () => {
+  it("renders correctly with different tag text", () => {
     render(<RecipeTag tag="Gluten-free" />);
     const tagElement = screen.getByText(/Gluten-free/i);
     expect(tagElement).toBeInTheDocument();
---
+++ src/components/Recipes/Tags/RecipeTagsGroup.jsx
@@ -0,0 +1,27 @@
+import RecipeTag from "./RecipeTag.jsx";
+import { useMemo } from "react";
+import PropTypes from "prop-types";
+
+export default function RecipeTagsGroup({ tags }) {
+  const tagsToPresent = useMemo(() => {
+    const tagsToPresent = tags.slice(0, 5);
+
+    if (tags.length > 5) tagsToPresent.push("...");
+
+    return tagsToPresent;
+  }, [tags]);
+
+  return (
+    <>
+      <div className="flex flex-row flex-wrap">
+        {tagsToPresent.map((tag, index) => (
+          <RecipeTag key={"RecipeTag_" + index} tag={tag} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+RecipeTagsGroup.propTypes = {
+  tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
