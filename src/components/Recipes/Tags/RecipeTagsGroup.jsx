0a7146cbce0e8a76124d653eb480ec511925e6ae
Hugo Gonçalves <hugomiguel_2001_@sapo.pt>
2023-11-29 16:50:08 +0000

    Included unit test for SearchResultItem component

--- src/components/Recipes/Tags/RecipeTagsGroup.jsx
+++ src/components/Recipes/Tags/RecipeTagsGroup.jsx
@@ -4,6 +4,8 @@ import PropTypes from "prop-types";
 
 export default function RecipeTagsGroup({ tags }) {
   const tagsToPresent = useMemo(() => {
+    if (!tags) return [];
+
     const tagsToPresent = tags.slice(0, 5);
 
     if (tags.length > 5) tagsToPresent.push("...");
@@ -23,5 +25,5 @@ export default function RecipeTagsGroup({ tags }) {
 }
 
 RecipeTagsGroup.propTypes = {
-  tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+  tags: PropTypes.arrayOf(PropTypes.string),
 };
