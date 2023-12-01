d9c357625533a5c546065474faad03213ee6a25c
Hugo Gonçalves <hugomiguel_2001_@sapo.pt>
2023-12-01 22:49:56 +0000

    Fixed broken unit tests

--- src/components/Recipes/Categories/RecipeCategory.test.jsx
+++ src/components/Recipes/Categories/RecipeCategory.test.jsx
@@ -1,13 +1,30 @@
 import { render, screen } from "@testing-library/react";
 import RecipeCategory from "./RecipeCategory.jsx";
+import { configureStore } from "@reduxjs/toolkit";
+import searchReducer from "../../../redux/slices/searchSlice.js";
+import { Provider } from "react-redux";
+import { Router } from "react-router-dom";
 
 describe("RecipeCategory", () => {
+
+  let store;
+  beforeEach(() => {
+    store = configureStore({
+      reducer: {
+        search: searchReducer,
+      },
+    });
+  });
+
   it("renders with provided category data", () => {
     render(
-      <RecipeCategory
-        categoryName="Test Category"
-        categoryImageURI="https://test.com/image.jpg"
-      />,
+      <Provider store={store}>
+        <Router location={"/"} navigator={null}>
+        <RecipeCategory
+          categoryName="Test Category"
+          categoryImageURI="https://test.com/image.jpg" />
+        </Router>
+      </Provider>,
     );
 
     expect(screen.getByText("Test Category")).toBeInTheDocument();
@@ -18,7 +35,13 @@ describe("RecipeCategory", () => {
   });
 
   it("renders with default data when no category data is provided", () => {
-    render(<RecipeCategory />);
+    render(
+      <Provider store={store}>
+        <Router location={"/"} navigator={null}>
+          <RecipeCategory />
+        </Router>
+      </Provider>,
+    );
 
     expect(screen.getByText("Category")).toBeInTheDocument();
     expect(screen.getByRole("img")).toHaveAttribute(
