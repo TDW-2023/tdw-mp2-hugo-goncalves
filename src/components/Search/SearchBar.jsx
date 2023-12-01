aa966d0d6ac75c35fea047e8d4cceafeea7226a9
Hugo Gonçalves <hugomiguel_2001_@sapo.pt>
2023-12-01 16:41:23 +0000

    Changed query fetch logic to SearchBar component

--- src/components/Search/SearchBar.jsx
+++ src/components/Search/SearchBar.jsx
@@ -1,14 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useGetRecipesFromSearchQuery } from "../../redux/slices/freeRecipesAPISlice.js";
+import { setSearchResults } from "../../redux/slices/searchSlice.js";
+import PropTypes from "prop-types";
 
-export default function SearchBar({query}) {
+export default function SearchBar({ query }) {
+  const [searchQuery, setSearchQuery] = useState(query);
+  const [shouldReFetch, setShouldReFetch] = useState(true);
 
-  const [searchQuery, setSearchQuery] = useState("");
-  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const selectedFilters = useSelector((state) => state.search.selectedFilters);
+
+  const nav = useNavigate();
+
+  const transformFilters = () => {
+    const filters = {};
+
+    selectedFilters.forEach((filter) => {
+      if (Object.prototype.hasOwnProperty.call(filters, filter.filterType.id)) {
+        filters[filter.filterType.id].push(filter.filterName);
+      } else {
+        filters[filter.filterType.id] = [filter.filterName];
+      }
+    });
+
+    return filters;
+  };
+
+  const { data } = useGetRecipesFromSearchQuery(
+    { query: searchQuery, filters: transformFilters(selectedFilters) },
+    { skip: !shouldReFetch },
+  );
+
+  useEffect(() => {
+    dispatch(setSearchResults(data));
+    if (data) setShouldReFetch(false); // Disable re-fetching
+  }, [data, dispatch]);
 
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
-      navigate("/search/" + searchQuery);
+      if (!searchQuery || searchQuery === "") {
+        console.log("No search query");
+        return;
+      }
+      setShouldReFetch(true); // Re-render the component and program a re-fetch on next render
+      nav(`/search/${searchQuery}`);
     }
   };
 
@@ -28,9 +65,11 @@ export default function SearchBar({query}) {
           <input
             type="text"
             placeholder="Search..."
-            value={query ? query : null}
             className="bg-gray-200 dark:bg-gray-800 w-full mx-6 border-none outline-none text-black focus:ring-0 dark:text-gray-300 text-lg"
-            onChange={(e) => setSearchQuery(e.target.value)}
+            defaultValue={searchQuery}
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
             onKeyDown={(e) => handleKeyDown(e)}
           />
         </div>
@@ -38,3 +77,7 @@ export default function SearchBar({query}) {
     </>
   );
 }
+
+SearchBar.propTypes = {
+  query: PropTypes.string.isRequired,
+};
